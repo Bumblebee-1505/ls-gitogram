3ad7448c2e62905f35f8261e223aa5507e959d12
Akmal <akmal15051993@gmail.com>
2023-08-22 16:56:11 +0500

    user item commit

--- src/pages/feeds/feeds.vue
+++ src/pages/feeds/feeds.vue
@@ -2,18 +2,31 @@
     <div class="topline">
         <topline>
             <template #headline>
-                <icon name="logo"/>
+                <div class="logo">
+                    <icon name="logo"/>
+                </div>
                 <div class="icon">
-                    <icon name="home"/>
-                    <icon name="home"/>
-                    <icon name="shape"/>
+                    <a href="" class="icon__link">
+                        <icon name="home"/>
+                    </a>
+                    <a href="" class="icon__link">
+                        <icon name="home"/>
+                    </a>
+                    <a href="" class="icon__link">
+                        <icon name="shape"/>
+                    </a>
                 </div>
             </template>
-        </topline>
-
-        <topline>
             <template #content>
-                <h2>Content</h2>
+                <ul class="stories">
+                    <li class="stories__item" v-for="story in stories" :key="story.id">
+                        <story-user-item
+                            :avatar="story.avatar"
+                            :username="story.username"
+                            @onPress="handlePress(story.id)"
+                        />
+                    </li>
+                </ul>
             </template>
         </topline>
 
@@ -22,22 +35,47 @@
 
 <script>
 import { topline } from '../../components/topline'
+import { storyUserItem } from '../../components/storyUserItem'
+import stories from './data.json'
 import { icon } from '../../icons'
 export default {
   name: 'feeds',
   components: {
     topline,
+    storyUserItem,
     icon
+  },
+  data () {
+    return {
+      stories
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
-    .icon {
-        color: black;
+    .logo {
+        color: #000;
+        width: 174px;
+        height: 40px;
     }
-    .icon:hover {
-        color: greenyellow;
+    .icon {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &__link {
+            width: 32px;
+            color: #262626 ;
+            margin-right: 20px;
+        }
+
+        &__link:last-child {
+            margin-right: 0;
+        }
+
+        &__link:hover {
+            color: #31AE54;
+        }
     }
 </style>
